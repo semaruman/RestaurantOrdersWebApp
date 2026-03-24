8f9e1a8bbf3bbb29a2480ffc68dd6daed4405729
Semen <semen.ruman@gmail.com>
2026-03-24 10:24:38 +0300

    Убрал лишний middleware в Program.cs

--- RestaurantOrdersWebApp/Program.cs
+++ RestaurantOrdersWebApp/Program.cs
@@ -3,20 +3,7 @@ using RestaurantOrdersWebApp.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Map("/fastfood", appBuilder =>
-{
-    app.UseRestaurantMiddleware();
-});
 
-app.Map("/delivery", appBuilder =>
-{
-    app.UseDeliveryRestaurantMiddleware();
-});
-
-app.Map("/premium", appBuilder =>
-{
-    app.UsePremiumRestaurantMiddleware();
-});
 
 app.Run(async (context) => await context.Response.WriteAsync("Добро пожаловать"));
 
