3112542e7956c7c4205a9242f83b2d4b6031a38d
Semen <semen.ruman@gmail.com>
2026-03-25 11:08:24 +0300

    Добавил интерфейс IRestaurantService для работы с рестораном

---
+++ RestaurantOrdersWebApp/Services/EF services/RestaurantService.cs
@@ -0,0 +1,11 @@
+﻿using RestaurantOrdersWebApp.Data;
+using RestaurantOrdersWebApp.Models;
+using RestaurantOrdersWebApp.Services.Interfaces;
+
+namespace RestaurantOrdersWebApp.Services.EF_services
+{
+    public class RestaurantService : IRestaurantService
+    {
+        
+    }
+}
---
+++ RestaurantOrdersWebApp/Services/Interfaces/IRestaurantService.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantOrdersWebApp.Data;
+using RestaurantOrdersWebApp.Models;
+
+namespace RestaurantOrdersWebApp.Services.Interfaces
+{
+    public interface IRestaurantService
+    {
+        public List<Restaurant> GetAllRestaurants();
+
+        public Restaurant GetRestaurantByName(string restaurantName);
+
+        public bool AddRestaurant(Restaurant restaurant);
+    }
+}
