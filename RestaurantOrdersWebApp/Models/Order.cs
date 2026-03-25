02bb84c4b61a4929deb7d7b07194d6d7d77dcb88
Semen <semen.ruman@gmail.com>
2026-03-25 10:28:38 +0300

    Добавил связб 1 к 1 для заказа и ресторана

--- RestaurantOrdersWebApp/Models/Order.cs
+++ RestaurantOrdersWebApp/Models/Order.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
 
+        public int RestaurantId { get; set; }
+
         public List<Dish> Dishes { get; set; }
 
         public string Status { get; set; }
