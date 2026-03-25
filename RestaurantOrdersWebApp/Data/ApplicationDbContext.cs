f274331710d447d50731f66d519b7ceba971f66e
Semen <semen.ruman@gmail.com>
2026-03-25 10:29:29 +0300

    Добавил поля таблиц в ApplicationDbContext

--- RestaurantOrdersWebApp/Data/ApplicationDbContext.cs
+++ RestaurantOrdersWebApp/Data/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RestaurantOrdersWebApp.Models;
 
 namespace RestaurantOrdersWebApp.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Restaurant> Restaurants { get; set; }
 
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Dish> Dishes {  get; set; }
     }
 }
